921981a2a12a0b4b79879d6e5cd0814d1ce82bcf
Mike <mike@MikeBook.localdomain>
2020-10-09 15:46:21 -0500

    test pd

--- bot.py
+++ bot.py
@@ -12,47 +12,32 @@ puntDF = pd.read_csv('surrender.csv')
 TOKEN = environ['DISCORD_TOKEN']
 
 bot = commands.Bot(command_prefix='s!')
-
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
-
-@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
-async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-
-    response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
-    
-@bot.command(name='roll_dice', help='Simulates rolling dice.')
-async def roll(ctx, number_of_dice: int, number_of_sides: int):
-    dice = [
-        str(random.choice(range(1, number_of_sides + 1)))
-        for _ in range(number_of_dice)
-    ]
-    await ctx.send(', '.join(dice))
     
 @bot.command(name='game', help='Get surrender index for a game ID.')
 async def game(ctx, game_id: int):
     gameDF = puntDF[(puntDF.gameID == game_id) & (puntDF.percentiles >= 90)]
-    await ctx.send("All 90th percentile punts from game %s (%i total):"%(len(gameDF),game_id))
+    
+    if num < 10:
+        strnum = '0' + str(num)
+    elif num >= 10:
+        strnum = str(num)
+    if num < 24:
+        urlprefix = "http://sim-football.com/indexes/NSFLS%s"%strnum
+    else:
+        urlprefix = "http://sim-football.com/indexes/ISFLS%s"%strnum
+    
+    await ctx.send("All 90th percentile punts from game %s (%i total):"%(game_id, len(gameDF)))
     for i in range(len(gameDF)):
         embed=discord.Embed(title=gameDF['play'].iloc[i],color=0xff7b00)
         embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/685587194861060146/731295955982483547/ISFL_logo_2000px.png")
-        embed.set_author(name="S%i - %s @ %s (%i/%i)"%(gameDF['S'].iloc[i],gameDF['awayTeam'].iloc[i],gameDF['homeTeam'].iloc[i],i,len(gameDF)))
+        embed.set_author(name="S%i - %s @ %s (%i/%i)"%(gameDF['S'].iloc[i],gameDF['awayTeam'].iloc[i],gameDF['homeTeam'].iloc[i],i+1,len(gameDF)),
+                         url="%s/Boxscores/%s.html"%(urlprefix,game_id))
         embed.add_field(name="Game Situation", value=gameDF['situation'].iloc[i], inline=False)
 #         embed.add_field(name=Punt, value=gameDF['play'].iloc[i], inline=False)
-        embed.add_field(name="Score", value=gameDF['surrenderIndex'].iloc[i], inline=False)
+        embed.add_field(name="Surrender Index", value=gameDF['surrenderIndex'].iloc[i], inline=False)
         embed.add_field(name="Overall Rank", value=gameDF['surrenderRank'].iloc[i], inline=True)
         embed.add_field(name="Percentile", value=round(gameDF['percentiles'].iloc[i],3), inline=True)
-        embed.set_footer(text="SurrenderBot by infinitempg")
+        embed.set_footer(text="SurrenderBot, by infinitempg")
         await ctx.send(embed=embed)
 
 

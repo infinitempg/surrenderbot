32e736d1d7fde8c6a8865d09bc8d6694f62f6c69
Mike <mike@MikeBook.localdomain>
2020-10-09 19:24:03 -0500

    gamefix

--- bot.py
+++ bot.py
@@ -27,14 +27,14 @@ async def help(ctx):
     await ctx.send(embed=embed)
     return
    
-@bot.command(name='game', help='Get surrender index for a game ID.')
-async def game(ctx, game_id: int):
+@bot.command(name='gameID', help='Get surrender index for a game ID.')
+async def gameID(ctx, game_id: int):
     gameDF = puntDF[(puntDF.gameID == game_id) & (puntDF.percentiles >= 90)]
 
     if len(gameDF) < 1:
         await ctx.send("Error - Game Not Found")
         return
-
+    
     num = gameDF['S'].iloc[0]
     if num < 10:
         strnum = '0' + str(num)
@@ -45,19 +45,33 @@ async def game(ctx, game_id: int):
     else:
         urlprefix = "http://sim-football.com/indexes/ISFLS%s"%strnum
 
-    await ctx.send("All 90th percentile punts from game %s (%i total):"%(game_id, len(gameDF)))
-    for i in range(len(gameDF)):
-        embed=discord.Embed(title=gameDF['play'].iloc[i],color=0xff7b00)
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/685587194861060146/731295955982483547/ISFL_logo_2000px.png")
-        embed.set_author(name="S%i - %s @ %s (%i/%i)"%(gameDF['S'].iloc[i],gameDF['awayTeam'].iloc[i],gameDF['homeTeam'].iloc[i],i+1,len(gameDF)),
-                         url="%s/Boxscores/%s.html"%(urlprefix,game_id))
-        embed.add_field(name="Game Situation", value=gameDF['situation'].iloc[i], inline=False)
-#         embed.add_field(name=Punt, value=gameDF['play'].iloc[i], inline=False)
-        embed.add_field(name="Surrender Index", value=round(gameDF['surrenderIndex'].iloc[i],3), inline=False)
-        embed.add_field(name="Overall Rank", value=gameDF['surrenderRank'].iloc[i], inline=True)
-        embed.add_field(name="Percentile", value=round(gameDF['percentiles'].iloc[i],3), inline=True)
-        embed.set_footer(text="SurrenderBot, by infinitempg")
-        await ctx.send(embed=embed)
+    await ctx.send("All 90th percentile punts from  (%i total):"%(game_id, len(gameDF)))
+    
+    gameDF = gameDF.rename(columns={'situation':'Game Situation','play':'Punt',
+                                    'surrenderIndex':"Index",'surrenderRank':'Rank',
+                                    'percentiles':"Perc."})
+    if len(gameDF) <= 10:
+        table = tabulate(gameDF[['Game Situation', 'Punt','Index','Rank','Perc.']],headers='keys',tablefmt='simple',showindex=False)
+        await ctx.send("```%s```"%table)
+    else:
+        game1DF, game2DF = np.split(gameDF, [int(.5*len(gameDF))])
+        table1 = tabulate(game1DF[['Game Situation', 'Punt','Index','Rank','Perc.']],headers='keys',tablefmt='simple',showindex=False)
+        table2 = tabulate(game2DF[['Game Situation', 'Punt','Index','Rank','Perc.']],headers='keys',tablefmt='simple',showindex=False)
+        await ctx.send("```%s```"%table1)
+        await ctx.send("```%s```"%table2)
+        
+#     for i in range(len(gameDF)):
+#         embed=discord.Embed(title=gameDF['play'].iloc[i],color=0xff7b00)
+#         embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/685587194861060146/731295955982483547/ISFL_logo_2000px.png")
+#         embed.set_author(name="S%i - %s @ %s (%i/%i)"%(gameDF['S'].iloc[i],gameDF['awayTeam'].iloc[i],gameDF['homeTeam'].iloc[i],i+1,len(gameDF)),
+#                          url="%s/Boxscores/%s.html"%(urlprefix,game_id))
+#         embed.add_field(name="Game Situation", value=gameDF['situation'].iloc[i], inline=False)
+# #         embed.add_field(name=Punt, value=gameDF['play'].iloc[i], inline=False)
+#         embed.add_field(name="Surrender Index", value=round(gameDF['surrenderIndex'].iloc[i],3), inline=False)
+#         embed.add_field(name="Overall Rank", value=gameDF['surrenderRank'].iloc[i], inline=True)
+#         embed.add_field(name="Percentile", value=round(gameDF['percentiles'].iloc[i],3), inline=True)
+#         embed.set_footer(text="SurrenderBot, by infinitempg")
+#         await ctx.send(embed=embed)
     return
   
 @bot.command(name='top', help='List of top Surrender Punts')

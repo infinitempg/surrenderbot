67f21ae308b4af42d821695624326ee0faaed76f
Mike <mike@MikeBook.localdomain>
2020-10-19 16:48:16 -0500

    tweeter

--- surrender.py
+++ surrender.py
@@ -105,6 +105,67 @@ def splitline(row):
         return str(split[0] + 'BLOCKED\n' + split[1])
     else:
         return row.play
+    
+def tweetMessage(row):
+
+    # location of ball
+    if row.dist2goal < 50:
+        loc = "%s %i"%(row.recTeam,row.dist2goal)
+    elif row.dist2goal == 50:
+        loc = "midfield"
+    else:
+        loc = "%s %i"%(row.teamPoss,100-row.dist2goal)
+
+    # who has what score
+    if row['awayTeam'] == row['teamPoss']:
+        puntScore = row['awayScore']
+        recScore = row['homeScore']
+    else:
+        puntScore = row['homeScore']
+        recScore = row['awayScore']
+
+    # score string
+    if puntScore < recScore:
+        rel = "losing"
+    elif puntScore > recScore:
+        rel = "winning"
+    else:
+        rel = "tied"
+
+    # percentile string
+    percentile = int(row.percentiles)
+    mod = percentile%10
+    if mod == 1:
+        perc = "%ist"%percentile
+    elif mod == 2:
+        perc = "%ind"%percentile
+    elif mod == 3:
+        perc = "%ird"%percentile
+    else:
+        perc = "%ith"%percentile
+
+    # down and distance
+    if row.down == 1:
+        dW = "%ist"%row.down
+    elif row.down == 2:
+        dW = "%ind"%row.down
+    elif row.down == 3:
+        dW = "%ird"%row.down
+    else:
+        dW = "%ith"%row.down
+
+    # quarter
+    if row.Q == 1:
+        Q = "%ist"%row.Q
+    elif row.Q == 2:
+        Q = "%ind"%row.Q
+    elif row.Q == 3:
+        Q = "%ird"%row.Q
+    else:
+        Q = "%ith"%row.Q
+
+    string = "In S%i W%i, %s decided to punt to %s from the %s on %s and %i with %s remaining in the %s quarter while %s %i to %i.\n\nWith a Surrender Index of %.2f, this punt ranks at the %s percentile of cowardly punts in ISFL History. Overall, it is ranked #%i all time."%(row.S,row.W,row.teamPoss,row.recTeam,loc,dW,row.distance,row.time,Q,rel,puntScore,recScore,row.surrenderIndex,perc,row.surrenderRank)
+    return string
 '''
 Get PBP
 '''
@@ -113,89 +174,86 @@ S = 25
 sData = []
 idList, idDict = getSeasonIDs(S)
 
-print('Season:',S)
-print('Games:',len(idList))
-
-print('Gathering PBP...')
-for i in idList:
-    sData.append(getGameData(S,i,idDict))
-
-sDF = pd.concat(sData)
-sDF.to_csv('PBP/S%sPBP.csv'%S)
-s3.upload_file('PBP/S%sPBP.csv'%S,'isfl-surrender-bot','PBP/S%sPBP.csv'%S)
-
-print('PBP for S%s saved!'%S)
-
-print('Combining all Seasons...')
-
-for i in range(1,S+1):
-    s3.download_file('isfl-surrender-bot','PBP/S%iPBP.csv'%i,'PBP/S%iPBP.csv'%i)
-
-import os
-allDF = pd.concat([pd.read_csv('PBP/%s'%p) for p in next(os.walk('PBP'))[2] if '.csv' in p])
-allDF = allDF.reset_index().sort_values('S')
-# allDF.to_csv('allPBP.csv')
-
-print('Getting Punts...')
-puntDF = allDF[allDF['play'].str.contains('Punt ')]
-
-print('Done!')
-
-'''
-Surrender Punts
-'''
-
-print('Cleaning up PuntDF...')
-NFL = np.load("2009-2019_surrender_indices.npy")
-puntDF['recTeam'] = puntDF.apply(lambda row : receiving(row),axis=1)
-puntDF['surrenderIndex'] = puntDF.apply(lambda row : surrenderIndex(row),axis=1)
-puntDF = puntDF[['S','W','gameID','Q','time','awayTeam','awayScore','homeScore','homeTeam'
-                 ,'teamPoss','recTeam','down','distance','side','dist2goal','surrenderIndex','play']]
-puntDF = puntDF.sort_values('surrenderIndex',ascending=False)
-puntDF['NFLpercentiles'] = puntDF.apply(lambda row : perc(NFL,row['surrenderIndex']),axis=1)
-puntDF['percentiles'] = puntDF.apply(lambda row : perc(puntDF['surrenderIndex'],row['surrenderIndex']),axis=1)
-puntDF['result'] = puntDF.apply(lambda row : touchback(row),axis = 1)
-puntDF['scoreDiff'] = puntDF.apply(lambda row : scoreDiff(row),axis = 1)
-puntDF['puntDist'] = puntDF.apply(lambda row : puntDist(row),axis = 1)
-puntDF['puntEndLoc'] = puntDF['dist2goal'] - puntDF['puntDist']
-puntDF['surrenderRank'] = puntDF.surrenderIndex.rank(method='max',ascending=False).astype('int')
-puntDF['situation'] = puntDF.apply(lambda row : stringify(row), axis = 1)
-puntDF['play'] = puntDF['play'].str.replace('Dasistwirklichseinnachname, A.','D, Alex')
-puntDF['play'] = puntDF.apply(lambda row : splitline(row), axis = 1)
-
-puntDF = puntDF[['S', 'W', 'gameID', 'Q', 'time', 'awayTeam', 'awayScore', 'homeScore',
-       'homeTeam', 'teamPoss', 'recTeam', 'down', 'distance', 'dist2goal', 'puntDist',
-       'puntEndLoc', 'surrenderIndex','surrenderRank', 'percentiles', 'NFLpercentiles', 'play',
-       'situation','result']]
-
-print('Exporting Surrender DF...')
-puntDF.to_csv('surrender.csv')
-s3.upload_file('surrender.csv','isfl-surrender-bot','surrender.csv')
-
-print('Done!')
-
-print('Restarting SurrenderBot')
-import heroku3
-heroku_conn = heroku3.from_key(environ['HEROKU_KEY'])
-app = heroku_conn.apps()['surrenderbot']
-app.restart()
-print('App Restarted!')
-
 print('Checking if new week...')
 s3.download_file('isfl-surrender-bot','curSW.csv','curSW.csv')
 curSW = pd.read_csv('curSW.csv').iloc[0]
 
-curS = max(testDF.S)
-curW = max(testDF[testDF.S == curS].W)
-print('S%iW%i'%(curS,curW))
-
-if curSW.S == curS and curSW.W == curW:
+if curSW.S == S and curSW.W == max(idDict.values()):
     print('No update.')
 else:
-    print('Tweeting top surrender punts...')
     newSW = pd.DataFrame([[curS,curW]],columns=['S','W'])
     newSW.to_csv('curSW.csv')
     s3.upload_file('curSW.csv','isfl-surrender-bot','curSW.csv')
+    
+    print('Season:',S)
+    print('Games:',len(idList))
+
+    print('Gathering PBP...')
+    for i in idList:
+        sData.append(getGameData(S,i,idDict))
+
+    sDF = pd.concat(sData)
+    sDF.to_csv('PBP/S%sPBP.csv'%S)
+    s3.upload_file('PBP/S%sPBP.csv'%S,'isfl-surrender-bot','PBP/S%sPBP.csv'%S)
+
+    print('PBP for S%s saved!'%S)
+
+    print('Combining all Seasons...')
+
+    for i in range(1,S+1):
+        s3.download_file('isfl-surrender-bot','PBP/S%iPBP.csv'%i,'PBP/S%iPBP.csv'%i)
+
+    import os
+    allDF = pd.concat([pd.read_csv('PBP/%s'%p) for p in next(os.walk('PBP'))[2] if '.csv' in p])
+    allDF = allDF.reset_index().sort_values('S')
+    # allDF.to_csv('allPBP.csv')
+
+    print('Getting Punts...')
+    puntDF = allDF[allDF['play'].str.contains('Punt ')]
+
+    print('Done!')
+
+    '''
+    Surrender Punts
+    '''
+
+    print('Cleaning up PuntDF...')
+    NFL = np.load("2009-2019_surrender_indices.npy")
+    puntDF['recTeam'] = puntDF.apply(lambda row : receiving(row),axis=1)
+    puntDF['surrenderIndex'] = puntDF.apply(lambda row : surrenderIndex(row),axis=1)
+    puntDF = puntDF[['S','W','gameID','Q','time','awayTeam','awayScore','homeScore','homeTeam'
+                     ,'teamPoss','recTeam','down','distance','side','dist2goal','surrenderIndex','play']]
+    puntDF = puntDF.sort_values('surrenderIndex',ascending=False)
+    puntDF['NFLpercentiles'] = puntDF.apply(lambda row : perc(NFL,row['surrenderIndex']),axis=1)
+    puntDF['percentiles'] = puntDF.apply(lambda row : perc(puntDF['surrenderIndex'],row['surrenderIndex']),axis=1)
+    puntDF['result'] = puntDF.apply(lambda row : touchback(row),axis = 1)
+    puntDF['scoreDiff'] = puntDF.apply(lambda row : scoreDiff(row),axis = 1)
+    puntDF['puntDist'] = puntDF.apply(lambda row : puntDist(row),axis = 1)
+    puntDF['puntEndLoc'] = puntDF['dist2goal'] - puntDF['puntDist']
+    puntDF['surrenderRank'] = puntDF.surrenderIndex.rank(method='max',ascending=False).astype('int')
+    puntDF['situation'] = puntDF.apply(lambda row : stringify(row), axis = 1)
+    puntDF['play'] = puntDF['play'].str.replace('Dasistwirklichseinnachname, A.','D, Alex')
+    puntDF['play'] = puntDF.apply(lambda row : splitline(row), axis = 1)
+
+    puntDF = puntDF[['S', 'W', 'gameID', 'Q', 'time', 'awayTeam', 'awayScore', 'homeScore',
+           'homeTeam', 'teamPoss', 'recTeam', 'down', 'distance', 'dist2goal', 'puntDist',
+           'puntEndLoc', 'surrenderIndex','surrenderRank', 'percentiles', 'NFLpercentiles', 'play',
+           'situation','result']]
+
+    print('Exporting Surrender DF...')
+    puntDF.to_csv('surrender.csv')
+    s3.upload_file('surrender.csv','isfl-surrender-bot','surrender.csv')
+
+    print('Done!')
+
+    print('Restarting SurrenderBot')
+    import heroku3
+    heroku_conn = heroku3.from_key(environ['HEROKU_KEY'])
+    app = heroku_conn.apps()['surrenderbot']
+    app.restart()
+    print('App Restarted!')
+
+    print('Tweeting top surrender punts...')
     import tweepy
     from os import environ
 
@@ -209,71 +267,11 @@ else:
 
     api = tweepy.API(auth)
 
-    def tweetMessage(row):
-
-        # location of ball
-        if row.dist2goal < 50:
-            loc = "%s %i"%(row.recTeam,row.dist2goal)
-        elif row.dist2goal == 50:
-            loc = "midfield"
-        else:
-            loc = "%s %i"%(row.teamPoss,100-row.dist2goal)
-
-        # who has what score
-        if row['awayTeam'] == row['teamPoss']:
-            puntScore = row['awayScore']
-            recScore = row['homeScore']
-        else:
-            puntScore = row['homeScore']
-            recScore = row['awayScore']
-
-        # score string
-        if puntScore < recScore:
-            rel = "losing"
-        elif puntScore > recScore:
-            rel = "winning"
-        else:
-            rel = "tied"
-
-        # percentile string
-        percentile = int(row.percentiles)
-        mod = percentile%10
-        if mod == 1:
-            perc = "%ist"%percentile
-        elif mod == 2:
-            perc = "%ind"%percentile
-        elif mod == 3:
-            perc = "%ird"%percentile
-        else:
-            perc = "%ith"%percentile
-
-        # down and distance
-        if row.down == 1:
-            dW = "%ist"%row.down
-        elif row.down == 2:
-            dW = "%ind"%row.down
-        elif row.down == 3:
-            dW = "%ird"%row.down
-        else:
-            dW = "%ith"%row.down
-
-        # quarter
-        if row.Q == 1:
-            Q = "%ist"%row.Q
-        elif row.Q == 2:
-            Q = "%ind"%row.Q
-        elif row.Q == 3:
-            Q = "%ird"%row.Q
-        else:
-            Q = "%ith"%row.Q
-
-        string = "In S%i W%i, %s decided to punt to %s from the %s on %s and %i with %s remaining in the %s quarter while %s %i to %i.\n\nWith a Surrender Index of %.2f, this punt ranks at the %s percentile of cowardly punts in ISFL History. Overall, it is ranked #%i all time."%(row.S,row.W,row.teamPoss,row.recTeam,loc,dW,row.distance,row.time,Q,rel,puntScore,recScore,row.surrenderIndex,perc,row.surrenderRank)
-        return string
-
     thisWeek = puntDF[(puntDF.S == curS) & (puntDF.W == curW) & (puntDF.percentiles >= 90)].sort_values("percentiles")
     thisWeek['tweet'] = thisWeek.apply(lambda x : tweetMessage(x),axis=1)
     for i in range(len(thisWeek)):
         api.update_status(thisWeek.tweet.iloc[i])
 
     print('Tweeted!')
+    
 print('All Done. Goodbye!')

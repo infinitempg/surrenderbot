f475e5a29c1b4874651beeca288c38ee7ce07804
Mike <mike@MikeBook.localdomain>
2020-10-19 16:34:35 -0500

    tweeter

--- surrender.py
+++ surrender.py
@@ -180,3 +180,100 @@ heroku_conn = heroku3.from_key(environ['HEROKU_KEY'])
 app = heroku_conn.apps()['surrenderbot']
 app.restart()
 print('App Restarted!')
+
+print('Checking if new week...')
+s3.download_file('isfl-surrender-bot','curSW.csv','curSW.csv')
+curSW = pd.read_csv('curSW.csv').iloc[0]
+
+curS = max(testDF.S)
+curW = max(testDF[testDF.S == curS].W)
+print('S%iW%i'%(curS,curW))
+
+if curSW.S == curS and curSW.W == curW:
+    print('No update.')
+else:
+    print('Tweeting top surrender punts...')
+    newSW = pd.DataFrame([[curS,curW]],columns=['S','W'])
+    newSW.to_csv('curSW.csv')
+    s3.upload_file('curSW.csv','isfl-surrender-bot','curSW.csv')
+    import tweepy
+    from os import environ
+
+    consumer_key = environ['CONSUMER_KEY']
+    consumer_secret = environ['CONSUMER_SECRET']
+    access_token = environ['ACCESS_TOKEN']
+    access_token_secret = environ['ACCESS_TOKEN_SECRET']
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token,access_token_secret)
+
+    api = tweepy.API(auth)
+
+    def tweetMessage(row):
+
+        # location of ball
+        if row.dist2goal < 50:
+            loc = "%s %i"%(row.recTeam,row.dist2goal)
+        elif row.dist2goal == 50:
+            loc = "midfield"
+        else:
+            loc = "%s %i"%(row.teamPoss,100-row.dist2goal)
+
+        # who has what score
+        if row['awayTeam'] == row['teamPoss']:
+            puntScore = row['awayScore']
+            recScore = row['homeScore']
+        else:
+            puntScore = row['homeScore']
+            recScore = row['awayScore']
+
+        # score string
+        if puntScore < recScore:
+            rel = "losing"
+        elif puntScore > recScore:
+            rel = "winning"
+        else:
+            rel = "tied"
+
+        # percentile string
+        percentile = int(row.percentiles)
+        mod = percentile%10
+        if mod == 1:
+            perc = "%ist"%percentile
+        elif mod == 2:
+            perc = "%ind"%percentile
+        elif mod == 3:
+            perc = "%ird"%percentile
+        else:
+            perc = "%ith"%percentile
+
+        # down and distance
+        if row.down == 1:
+            dW = "%ist"%row.down
+        elif row.down == 2:
+            dW = "%ind"%row.down
+        elif row.down == 3:
+            dW = "%ird"%row.down
+        else:
+            dW = "%ith"%row.down
+
+        # quarter
+        if row.Q == 1:
+            Q = "%ist"%row.Q
+        elif row.Q == 2:
+            Q = "%ind"%row.Q
+        elif row.Q == 3:
+            Q = "%ird"%row.Q
+        else:
+            Q = "%ith"%row.Q
+
+        string = "In S%i W%i, %s decided to punt to %s from the %s on %s and %i with %s remaining in the %s quarter while %s %i to %i.\n\nWith a Surrender Index of %.2f, this punt ranks at the %s percentile of cowardly punts in ISFL History. Overall, it is ranked #%i all time."%(row.S,row.W,row.teamPoss,row.recTeam,loc,dW,row.distance,row.time,Q,rel,puntScore,recScore,row.surrenderIndex,perc,row.surrenderRank)
+        return string
+
+    thisWeek = testDF[(testDF.S == curS) & (testDF.W == curW) & (testDF.percentiles >= 90)].sort_values("percentiles")
+    thisWeek['tweet'] = thisWeek.apply(lambda x : tweetMessage(x),axis=1)
+    for i in range(len(thisWeek)):
+        api.update_status(thisWeek.tweet.iloc[i])
+
+    print('Tweeted!')
+print('All Done. Goodbye!')
